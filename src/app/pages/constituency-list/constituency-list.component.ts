6c9647670fb1b82c7dd551d258bdd6b6e8f29d62
ChakriNaidu31 <chakradharallamm@gmail.com>
2024-04-18 03:13:34 +0530

    remaining pages

--- src/app/pages/constituency-list/constituency-list.component.ts
+++ src/app/pages/constituency-list/constituency-list.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, QueryList, ViewChildren} from '@angular/core';
+import {DecimalPipe} from '@angular/common';
+import {Observable} from 'rxjs';
+import {VotersService} from '../../services/voters.service';
+import {Country} from '../../services/country';
+import {NgbdSortableHeader, SortEvent} from '../../services/sortable.directive';
+
 
 @Component({
   selector: 'app-constituency-list',
   templateUrl: './constituency-list.component.html',
-  styleUrls: ['./constituency-list.component.css']
+  styleUrls: ['./constituency-list.component.css'],
+  providers: [VotersService, DecimalPipe]
 })
-export class ConstituencyListComponent {
 
+
+export class ConstituencyListComponent {
+  countries$: Observable<Country[]>;
+  total$: Observable<number>;
+
+  @ViewChildren(NgbdSortableHeader) headers!: QueryList<NgbdSortableHeader>;
+
+  constructor(public service: VotersService) {
+    this.countries$ = service.countries$;
+    this.total$ = service.total$;
+  }
+
+  onSort({column, direction}: SortEvent) {
+
+    // resetting other headers
+    this.headers.forEach(header => {
+      if (header.sortable !== column) {
+        header.direction = '';
+      }
+    });
+
+    this.service.sortColumn = column;
+    this.service.sortDirection = direction;
+  }
 }
