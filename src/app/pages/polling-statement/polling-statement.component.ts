fc943f070f9f79295f221c24742a890e2cfd1084
ChakriNaidu31 <chakradharallamm@gmail.com>
2024-04-22 11:33:15 +0530

    validation

--- src/app/pages/polling-statement/polling-statement.component.ts
+++ src/app/pages/polling-statement/polling-statement.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+// polling-statement.component.ts
+
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-polling-statement',
   templateUrl: './polling-statement.component.html',
   styleUrls: ['./polling-statement.component.css']
 })
-export class PollingStatementComponent {
+export class PollingStatementComponent implements OnInit {
+  myForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.myForm = this.formBuilder.group({
+      ballotsIssued: ['', Validators.required],
+      serialNumberRange: ['', Validators.required],
+      rangeHundred: ['', Validators.required],
+      range50: ['', Validators.required],
+      range25: ['', Validators.required],
+      range10: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.myForm.invalid) {
+      this.markFormGroupTouched(this.myForm);
+      return;
+    }
+    // Handle form submission
+    console.log('Form submitted:', this.myForm.value);
+  }
+
+  markFormGroupTouched(formGroup: FormGroup) {
+    Object.values(formGroup.controls).forEach(control => {
+      control.markAsTouched();
+
+      if (control instanceof FormGroup) {
+        this.markFormGroupTouched(control);
+      }
+    });
+  }
 }
