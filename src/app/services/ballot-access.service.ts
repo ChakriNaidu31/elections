322bc39612b57594e02d891e3f4b9f0c09c8fddb
sasidharan <sasidharan.itprof@gmail.com>
2024-05-08 00:21:07 +0530

    Code cleanup

--- src/app/services/ballot-access.service.ts
+++ src/app/services/ballot-access.service.ts
@@ -135,20 +135,11 @@ export class BallotAccessService {
   getUserList() {
     return this.http.get(this.apiUrl + '/user', { headers: this.getHttpHeaders() });
   }
-  getUserById(id: string) {
-    return this.http.get(this.apiUrl + '/user/' + id, { headers: this.getHttpHeaders() });
-  }
-  getMyDetails() {
-    return this.http.get(this.apiUrl + '/user/single', { headers: this.getHttpHeaders() });
-  }
   createUser(user: any) {
     return this.http.post(this.apiUrl + '/user', user, { headers: this.getHttpHeaders() });
   }
   createAdminUser(user: any) {
     return this.http.post(this.apiUrl + '/user/createAdmin', user, { headers: this.getHttpHeaders() });
-  }
-  deactivateUser(id: string) {
-    return this.http.delete(this.apiUrl + '/user/' + id, { headers: this.getHttpHeaders() });
   }
 
   // Poll APIs
